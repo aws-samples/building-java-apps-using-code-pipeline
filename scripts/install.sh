d33cd707fa5113b541a83b38f2318252ee6af263
Nikunj Vaidya <vnikunj@amazon.com>
2020-11-02 09:54:14 -0800

    Correcting the scripts & sanitizing the repo

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,4 @@
 # Download the latest artifact from code artifact
 set -ex
 rm -rf /home/ec2-user/server/demoapplication.jar
-aws codeartifact get-package-version-asset --region=us-west-2 --domain mycdkdemoapp --repository mycdkdemoapp --format maven --package DemoApplication --namespace org.example --package-version 1.6 --asset DemoApplication-1.6-jar-with-dependencies.jar /home/ec2-user/server/demoapplication.jar > /tmp/demoutput
+aws codeartifact get-package-version-asset --region=us-west-2 --domain mydomain --repository mycdkdemoapp --format maven --package DemoApplication --namespace org.example --package-version 1.6 --asset DemoApplication-1.6-jar-with-dependencies.jar /home/ec2-user/server/demoapplication.jar > /tmp/demooutput 2>&1
--- scripts/start.sh
+++ scripts/start.sh
@@ -3,9 +3,9 @@ set -ex
 export DEMO_APP_BUCKET_NAME=<bucket_name>
 export AWS_CODEGURU_TARGET_REGION=<region>
 export DEMO_APP_SQS_URL=<https_sqs_url>
-export AWS_CODEGURU_PROFILER_GROUP_NAME=myDemoApp0911-WithIssues
+export AWS_CODEGURU_PROFILER_GROUP_NAME=myCodeGuruProfilingGroup-WithIssues
 ls
 pwd
 cd /home/ec2-user/server/
-nohup java -javaagent:/home/ec2-user/codeguru-profiler-java-agent-standalone-1.0.0.jar -jar /home/ec2-user/server/demoapplication.jar with-issues > /tmp/app.log 2>&1 &
+java -javaagent:/home/ec2-user/codeguru-profiler-java-agent-standalone-1.0.0.jar -jar /home/ec2-user/server/demoapplication.jar with-issues &>/dev/null &
 echo "do nothing"
