ce55a81c7329c8bada6ea92f4944734ff7770fd3
Nikunj Vaidya <vnikunj@amazon.com>
2021-05-31 12:49:19 -0700

    Updated the s3 delete script

--- delete-s3-bucket.py
+++ delete-s3-bucket.py
@@ -6,10 +6,12 @@
 BUCKET = '<s3-bucket>' 
 
 import boto3
-
+import time
 s3 = boto3.resource('s3')
 bucket = s3.Bucket(BUCKET)
 bucket.object_versions.delete()
+print("S3 bucket all object versions delete API invoked")
+time.sleep(3)
 
 # delete the now-empty bucket as well, uncomment this line:
 bucket.delete()
